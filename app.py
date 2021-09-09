17d298c2a59d94f1b937c39bde3720329e35f8af
Gausberg <philadelphiagaus@gmail.com>
2021-09-09 14:40:55 +0200

    add cors header

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import time
 
 app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route("/bot", method=["POST"])
+@cross_origin()
 
 # response
 def response():
     query = dict(request.form)['query']
-    result = query + "" + time.ctime()
+    result = query + " " + time.ctime()
     return jsonify({"response" : result})
 
 if __name__ == "__main__" : 
