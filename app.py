401bde515c903e7eb1d56a78cea70019dc5d1b13
Gausberg <philadelphiagaus@gmail.com>
2021-09-17 09:53:41 +0200

    cors rules

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import time
 
 app = Flask(__name__)
+CORS(app)
 
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/bot", method=["POST"])
-@cross_origin()
-
-# response
-def response():
-    query = dict(request.form)['query']
-    result = query + " " + time.ctime()
-    return jsonify({"response" : result})
-
-if __name__ == "__main__" : 
-       app.run(host="0.0.0.0", )
+@app.route("/api", methods=['GET'])
+def hello_world():
+    
+    d = {}      
+    d['Query'] = str(request.args['Query'])
+    return jsonify(d)
+   
+if __name__ == '__main__':
+    app.run()
